338596063b23c1c98ea3b723d98a8edef1f3749b
Meitar Moscovitz <meitarm@gmail.com>
2013-09-02 01:55:49 -0400

    Implement "Information from within your network" on dashboard.

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -6,13 +6,47 @@
 // * Show list of my friends against whom there ARE reports.
 
 // Initialize.
-$lists = array();
+$db = new PATFacebookDatabase();
+$reports_about_friends = array();
+$reports_about_reported = array();
 
-// Search for any reports which have friends as their reportee.
-if ($results) {
-    $lists['reported_friends'] = array();
-    foreach ($results as $reportee) {
-        $lists['reported_friends'][] = $reportee;
+// Search for any reports against this user's friends.
+$sql_vals = array();
+$sql = 'SELECT id, reportee_id FROM incidents WHERE reportee_id IN (';
+$i = 1;
+foreach ($friends as $friend) {
+    $sql .= "\$$i"; // Bind query parameter position.
+    if ($i !== count($friends)) {
+        $sql .= ','; // Only add trailing comma if not last time through loop.
+    }
+    array_push($sql_vals, $friend['id']); // Add value to array.
+    $i++;
+}
+$sql .= ');';
+$result = pg_query_params($db->connect(psqlConnectionStringFromDatabaseUrl()),
+    $sql,
+    $sql_vals
+);
+if (pg_num_rows($result)) {
+    while ($row = pg_fetch_object($result)) {
+        foreach ($friends as $friend) {
+            if ($friend['id'] == $row->reportee_id) {
+                $friend['pat_report_id'] = $row->id;
+                $reports_about_friends[] = $friend;
+            }
+        }
+    }
+}
+
+// Search for any reports filed by other people about people I've reported.
+$sql_vals = array();
+$sql  = 'SELECT id, reportee_id FROM incidents WHERE reportee_id IN (';
+$sql .= 'SELECT reportee_id FROM incidents WHERE reporter_id=$1';
+$sql .= ') AND reporter_id <> $1';
+$result = pg_query_params($db->getHandle(), $sql, array($user_id));
+if (pg_num_rows($result)) {
+    while ($row = pg_fetch_object($result)) {
+        $reports_about_reported[] = $row;
     }
 }
 ?>
@@ -46,47 +80,35 @@ if ($results) {
 </section>
 
 <section id="samples" class="clearfix">
-    <h1>Information from within your social network</h1>
+    <h1>Information from within your network</h1>
 
     <div class="list">
-        <h3>Friends without reports</h3>
+        <h3>Reports filed by others about people you've reported</h3>
+        <?php if ($reports_about_reported) { ?>
         <ul class="friends">
-        <?php
-        foreach ($friends as $friend) {
-        // Extract the pieces of info we need from the requests above
-        $id = idx($friend, 'id');
-        $name = idx($friend, 'name');
-        ?>
-            <!--          <li>
-                <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
-                    <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
-                    <?php echo he($name); ?>
-                </a>
-            </li>-->
-        <?php
-        }
-        ?>
+        <?php foreach ($reports_about_reported as $x) : ?>
+            <li>
+                <a href="reports.php?action=lookup&amp;id=<?php print he($x->id);?>"><img alt="" src="https://graph.facebook.com/<?php print he($x->reportee_id);?>/picture?type=square" /> View this report.<a/>
+            </li>
+        <?php endforeach; ?>
         </ul>
-        <p><a href="TK_LINK">See all</a></p>
+        <!--<p><a href="TK_LINK">See all</a></p>-->
+        <?php } else { ?>
+        <p>No reports found.</p>
+        <?php } ?>
     </div>
 
     <div class="list">
-        <h3>Friends with reports</h3>
-        <?php if ($lists['reported_friends']) { ?>
+        <h3>Reports filed about your Facebook friends</h3>
+        <?php if ($reports_about_friends) { ?>
         <ul class="friends">
-        <?php
-        foreach ($lists['reported_friends'] as $friend) :
-        // Extract the pieces of info we need from the requests above
-//        $id = idx($friend, 'id');
-//        $name = idx($friend, 'name');
-            $id = $friend;
-        ?>
+        <?php foreach ($reports_about_friends as $x) : ?>
             <li>
-                <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($id); ?>">
+                <a href="reports.php?action=lookup&amp;id=<?php print he($x['pat_report_id']);?>"><img src="https://graph.facebook.com/<?php echo he($x['id']) ?>/picture?type=square" alt=""> View this report about <?php print he($x['name']);?></a>
             </li>
         <?php endforeach;?>
         </ul>
-        <p><a href="TK_LINK">See all</a></p>
+        <!--<p><a href="TK_LINK">See all</a></p>-->
         <?php } else { ?>
         <p>No reports found.</p>
         <?php } ?>
