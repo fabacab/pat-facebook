fa0419f8b3d6e74d84cc21309b1c67655364aa7f
unquietpirate <foxtale@riseup.net>
2013-09-09 00:14:32 -0600

    Explain PAT-FB in more detail to users who are not logged in

--- templates/splash.php
+++ templates/splash.php
@@ -1,9 +1,6 @@
 <section id="get-started">
-    <p>The Predator Alert Tool for Facebook enables you to send and receive warnings to and from your friends about people in your immediate and extended Facebook social network who may be dangerous. This tool is designed for survivors of sexual assault and rape. <a href="#get-started-help" style="color:green;text-decoration:underline;">Click here to learn more</a>.</p>
+    <p>The Predator Alert Tool for Facebook is designed for survivors of sexual assault and rape. It allows you to share information about people in your social network who may be dangerous without having to reveal your identity. You can:<ul><<li><b>Talk about it.</b> Contribute your story with as much or as little detail as you feel comfortable sharing.<li><b>Decide who knows.</b> Control who gets to see your story and who doesn't. Display your identity only to the people you choose.<li><b>Get support.</b> Connect with friends who have had a bad experience with the same person you did.<li><b>Hear about it.</b> Read others' stories and find out what's being shared about people you know.</ul><a href="#get-started-help" style="color:green;text-decoration:underline;">Click here to learn more</a>.</p>
     <div class="fb-login-button">Log in to start using Predator Alert Tool for Facebook</div>
 </section>
 <section id="get-started-help">
-    <ol>
-        <li>TK: TODO - Explain this in more detail to new users.</li>
-    </ol>
 </section>
