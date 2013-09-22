7b2bec54db525df3a5e4560639038216e3fd1a57
Meitar Moscovitz <meitarm@gmail.com>
2013-09-22 12:38:52 -0400

    Fixed #38: Global navigation language is more succinct.

--- templates/global_navigation.php
+++ templates/global_navigation.php
@@ -1,8 +1,8 @@
 <nav class="GlobalNavigation">
     <ul>
         <li<?php if ($_SERVER['PHP_SELF'] === '/index.php') : ?> class="active"<?php endif;?>><a href="/">Dashboard</a></li>
-        <li<?php if ($_SERVER['PHP_SELF'] === '/reports.php' && $_GET['action'] === 'lookup') : ?> class="active"<?php endif;?>><a href="/reports.php?action=lookup">Find stories</a></li>
-        <li<?php if ($_SERVER['PHP_SELF'] === '/reports.php' && $_GET['action'] === 'new') : ?> class="active"<?php endif;?>><a href="/reports.php?action=new">Share your story</a></li>
+        <li<?php if ($_SERVER['PHP_SELF'] === '/reports.php' && $_GET['action'] === 'lookup') : ?> class="active"<?php endif;?>><a href="/reports.php?action=lookup">Search</a></li>
+        <li<?php if ($_SERVER['PHP_SELF'] === '/reports.php' && $_GET['action'] === 'new') : ?> class="active"<?php endif;?>><a href="/reports.php?action=new">Share</a></li>
         <li<?php if ($_SERVER['PHP_SELF'] === '/preferences.php') : ?> class="active"<?php endif;?>><a href="/preferences.php">Preferences</a></li>
         <li><a href="https://github.com/meitar/pat-facebook/wiki#user-manual" target="_blank">Help</a></li>
     </ul>
