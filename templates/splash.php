2a20e2857e3d59924be59428ec8c382eabb98c9f
Meitar Moscovitz <meitarm@gmail.com>
2013-09-26 22:17:18 -0400

    Link splash page bullet points to user manual pages.

--- templates/splash.php
+++ templates/splash.php
@@ -4,10 +4,10 @@
     <div class="description">
         <p>Using <?php print he($FBApp->getAppName());?>, you can:</p>
         <ul>
-            <li><strong>Talk about it.</strong> Contribute your story with as much or as little detail as you feel comfortable sharing.</li>
-            <li><strong>Decide who knows.</strong> Control who gets to see your story and who doesn't. Display your identity only to the people you choose.</li>
-            <li><strong>Get support.</strong> Connect with friends who have had a bad experience with the same person you did.</li>
-            <li><strong>Hear about it.</strong> Find out about others' bad experiences with people you know.</li>
+            <li><a href="<?php print he(DOCUMENTATION_URL_BASE);?>/User-Manual:Talk-About-It">Talk about it.</a> Contribute your story with as much or as little detail as you feel comfortable sharing.</li>
+            <li><a href="<?php print he(DOCUMENTATION_URL_BASE);?>/User-Manual:Decide-Who-Knows">Decide who knows.</a> Control who gets to see your story and who doesn't. Display your identity only to the people you choose.</li>
+            <li><a href="<?php print he(DOCUMENTATION_URL_BASE);?>/User-Manual:Get-Support">Get support.</a> Connect with friends who have had a bad experience with the same person you did.</li>
+            <li><a href="<?php print he(DOCUMENTATION_URL_BASE);?>/User-Manual:Hear-About-It">Hear about it.</a> Find out about others' bad experiences with people you know.</li>
         </ul>
     </div>
 </section>
